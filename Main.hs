6a37dce9838d272e6a8b7ab0a852f146b3a5afa6
Connor James <connornjames@gmail.com>
2020-11-09 17:10:06 -0800

    initial commit

---
+++ Main.hs
@@ -0,0 +1,112 @@
+{-# language LambdaCase #-}
+{-# language ScopedTypeVariables #-}
+
+module Main where
+
+import Data.Colour.SRGB
+import Diagrams.Prelude
+import Diagrams.Backend.SVG
+import Diagrams.TwoD.Layout.Grid
+import Diagrams.TwoD.Image
+import Diagrams.TwoD.Size
+import Diagrams.TwoD.Text
+
+cardWidth :: Double
+cardWidth = 474
+
+-- red = 1
+-- blue = 2
+-- yellow = 3
+-- green = 4
+-- brown = 5
+-- purple = 6
+
+main :: IO ()
+main = do
+    redImg <- loadImage "../scapegoat_joe.png"
+    blueImg <- loadImage "../scapegoat_bill.png"
+    yellowImg <- loadImage "../scapegoat_outsourced.png"
+    brownImg <- loadImage "../scapegoat_brad.png"
+    purpleImg <- loadImage "../scapegoat_lance.png"
+
+    let red = Goat "joe" redImg (sRGB24 0xED 0x24 0x29)
+    let blue = Goat "bill" blueImg (sRGB24 0x17 0xA3 0xDD)
+    let yellow = Goat "outsourced" yellowImg (sRGB24 0xF4 0xEB 0x22)
+    -- TODO
+    let green = yellow
+    let brown = Goat "brad" brownImg (sRGB24 0xAC 0x6C 0x29)
+    let purple = Goat "lance" purpleImg (sRGB24 0xA1 0x79 0xB2)
+
+    let goats = [red,blue,yellow,green,brown,purple]
+
+    -- card matrix
+    renderSVG "deck.svg" nosize (mkCardRows goats)
+    renderSVG "back.svg" nosize (oneGoatCard red)
+
+    -- six-goat card
+    renderSVG "six.svg" nosize (sixGoatCard red blue yellow green brown purple)
+
+    -- placemats
+    stepsImg <- loadImage "../turnsteps.png"
+    renderSVG "placemat_red.svg" nosize $ mkPlacemat stepsImg red
+    renderSVG "placemat_blue.svg" nosize $ mkPlacemat stepsImg blue
+    renderSVG "placemat_yellow.svg" nosize $ mkPlacemat stepsImg yellow
+    renderSVG "placemat_green.svg" nosize $ mkPlacemat stepsImg green
+    renderSVG "placemat_brown.svg" nosize $ mkPlacemat stepsImg brown
+    renderSVG "placemat_purple.svg" nosize $ mkPlacemat stepsImg purple
+
+loadImage :: FilePath -> IO (Diagram SVG)
+loadImage path = loadImageEmb path >>= either (ioError . userError) (pure . image)
+
+nosize :: Num n => SizeSpec V2 n
+nosize = mkSizeSpec2D Nothing Nothing
+
+data Goat = Goat
+    { goatName :: String
+    , goatImage :: Diagram SVG
+    , goatColor :: Colour Double
+    }
+
+instance Eq Goat where
+    (Goat n _ _) == (Goat n' _ _) = n == n'
+
+---------- Placemats
+
+mkPlacemat :: Diagram SVG -> Goat -> Diagram SVG
+mkPlacemat steps goat = content `atop` padder
+    where
+        content = ((steps # scaleUToY 800) ||| strutX 50 ||| (goatImage goat # scaleUToY 600)) # center
+        padder = (rect (width content + 100) (height content + 100) :: Diagram SVG) # fillColor (goatColor goat) # lineWidth 0 # center
+
+
+---------- Card matrix (one- and two-goat cards)
+
+mkCardRows :: [Goat] -> Diagram SVG
+mkCardRows goats = vcat [mkCardRow goats goat | goat <- goats]
+
+mkCardRow :: [Goat] -> Goat -> Diagram SVG
+mkCardRow goats goat = hcat $ map (mkCard goat) goats
+
+mkCard :: Goat -> Goat -> Diagram SVG
+mkCard cur other
+  | cur == other = oneGoatCard cur
+  | otherwise = twoGoatCard cur other
+
+oneGoatCard :: Goat -> Diagram SVG
+oneGoatCard goat = scaled `atop` padder
+    where
+        scaled = goatImage goat # scaleUToX cardWidth
+        padder = (rect (width scaled) (height scaled * 2) :: Diagram SVG) # fillColor (goatColor goat) # lineWidth 0 # center
+
+twoGoatCard :: Goat -> Goat -> Diagram SVG
+twoGoatCard top bottom = (goatImage top === goatImage bottom) # scaleUToX cardWidth # center
+
+
+---------- six-goat card
+
+sixGoatCard :: Goat -> Goat -> Goat -> Goat -> Goat -> Goat -> Diagram SVG
+sixGoatCard one two three four five six = overlayed `atop` padder
+    where
+        overlayed = ((goatImage one ||| goatImage two) === (goatImage three ||| goatImage four) === (goatImage five ||| goatImage six)) # scaleUToX cardWidth # center
+        scaled = goatImage one # scaleUToX cardWidth
+        padder = (rect (width scaled) (height scaled * 2) :: Diagram SVG) # fillColor black # lineWidth 0 # center
---
+++ topng.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -x
+
+convert deck.svg deck.png
+convert back.svg back.png
+
+convert six.svg six.png
+
+convert placemat_red.svg placemat_red.png
+convert placemat_blue.svg placemat_blue.png
+convert placemat_yellow.svg placemat_yellow.png
+convert placemat_green.svg placemat_green.png
+convert placemat_brown.svg placemat_brown.png
+convert placemat_purple.svg placemat_purple.png
